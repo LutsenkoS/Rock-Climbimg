ddec20a97bd4b7c61ff2874e8671b7baa763cce7
Sergey Lutsenko <lutsenkosrg@gmail.com>
2018-04-13 14:35:02 +0300

    Code refactoring

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -4,14 +4,20 @@ public class MovementController : MonoBehaviour
 {
     public float Torque = 150f;
     public float Acceleration = 100f;
-   
-    public Transform Handle;
+
     public Transform Body;
+    public Transform Handle;
+    public Transform Hammer;
 
     private float _angle;
     private float _moveHor;
     private float _moveVer;
 
+    private Rigidbody handle;
+    void Start()
+    {
+        handle = Handle.GetComponent<Rigidbody>();
+    }
     private void FixedUpdate()
     {
         Move();
@@ -23,8 +29,8 @@ public class MovementController : MonoBehaviour
         _moveVer = Input.GetAxis("Vertical");
         if (_moveHor != 0 || _moveVer != 0)
         {                       
-            Vector3 angle = Vector3.Cross(transform.position - Body.position, new Vector3(_moveHor, _moveVer));
-            Handle.GetComponent<Rigidbody>().AddTorque(angle * Torque);
+            Vector3 angle = Vector3.Cross(Hammer.position - Body.position, new Vector3(_moveHor, _moveVer));
+            handle.AddTorque(angle * Torque);
         }
     }   
     private void OnCollisionEnter(Collision other)
